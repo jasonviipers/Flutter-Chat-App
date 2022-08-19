b141b67d574ed44b4080cc740ce510e6da997086
J4SON <jason@chatr.fr>
2022-08-19 11:18:37 +0200

    add colors var

---
+++ lib/Configs/Colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+final bgColors = Color(0xff3e474d);
+final colors = Color(0xffefefef);
+final themeColor = Color(0xff3e474d);
+final primaryColor = Color(0xff3e474d);
+final greyColor = Color(0xffaeaeae);
+final greyColor2 = Color(0xffE8E8E8);
+final white = Color(0xffE8E8E8);
